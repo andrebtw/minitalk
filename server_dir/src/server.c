8c569c6b6d6a4606b1b2b8b87fefc99593d00b1b
andrebtw <gwandrewgd@gmail.com>
2023-02-21 15:37:04 +0100

    Signals starts

--- server_dir/src/server.c
+++ server_dir/src/server.c
@@ -6,36 +6,44 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 13:46:03 by anrodri2          #+#    #+#             */
-/*   Updated: 2023/02/21 11:26:41 by anrodri2         ###   ########.fr       */
+/*   Updated: 2023/02/21 15:12:42 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 #include <signal.h>
 
-static void pSigHandler(int signo){
-    if (signo == SIGUSR1)
+int	index_code;
+
+static void	pSigHandler(int signo)
+{
+	static char	*string;
+
+	if (!string)
+		string = ft_calloc(1, sizeof(char));
+	if (!string)
 	{
-		ft_printf("0");
-    }
-	else
-	{
-		ft_printf("1");
+		index_code = MALLOC_FAILURE;
+		return ;
 	}
+	if ()
+	
 }
 
 int	main(void)
 {
+	index_code = 0;
 	ft_printf("%d\n", (getpid()));
-	struct sigaction sa;
-	sa.sa_flags = SA_RESTART;
-    signal.sa_handler = &pSigHandler;
+	struct sigaction signal;
+	signal.sa_flags = SA_RESTART;
+	signal.sa_handler = &pSigHandler;
     sigaction(SIGUSR1, &signal, NULL);
-	ft_printf("teyeye");
+	sigaction(SIGUSR2, &signal, NULL);
 	while (1)
 	{
-		/* code */
+		pause();
+		if (index_code == MALLOC_FAILURE)
+			return (EXIT_FAILURE);
 	}
-	ft_printf("test");
 	return (EXIT_SUCCESS);
 }
